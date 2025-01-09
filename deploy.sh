8d90b16dd3630a1b266ea5f7a5c4f292e8000b09
John <duanjohn99@gmail.com>
2025-01-08 21:36:21 -0500

    I think we got into the server but we're going to run a test to maek sure

--- deploy.sh
+++ deploy.sh
@@ -7,11 +7,11 @@ SERVER_USER=$2
 SERVER_PRIVATE_KEY=$3
 
 echo "Received arguments"
-echo SERVER_HOST
 
 ssh -o StrictHostKeyChecking=no -i <(echo "$SERVER_PRIVATE_KEY") "$SERVER_USER"@"$SERVER_HOST"
 
-cd /root/Celia/CeliaSite
-mkdir RANDOMTESTDIRECTORY
+echo "we're in"
+echo "current directory is: $(pwd)"
+touch testfile.txt
 
 exit 0
