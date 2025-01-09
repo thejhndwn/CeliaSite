4436630c930c9bbc0d6ffc2256fa4164213d5881
John <duanjohn99@gmail.com>
2025-01-08 21:25:17 -0500

    testing if the script is picking up the secrets and ssh in properly

--- deploy.sh
+++ deploy.sh
@@ -2,6 +2,16 @@
 
 set -e
 
-SERVER_HOST=${{ secrets.SERVER_HOST }}
+SERVER_HOST=$1
+SERVER_USER=$2
+SERVER_PRIVATE_KEY=$3
+
+echo "Received arguments"
 echo SERVER_HOST
-echo "success"
+
+ssh -o StrictHostKeyChecking=no -i <(echo "$SERVER_PRIVATE_KEY") "$SERVER_USER"@"$SERVER_HOST"
+
+cd /root/Celia/CeliaSite
+mkdir RANDOMTESTDIRECTORY
+
+exit 0
